af747428b05e4b157e19b4a3704af1d612117439
ruimalheiro <rmpmalheiro@gmail.com>
2013-11-08 17:10:18 +0000

    Small edit to trim the space over the iterator value.

--- installer-source/getversion.bat
+++ installer-source/getversion.bat
@@ -1,8 +1,7 @@
 @echo off
 cd ..\ka-lite\kalite
-for /f "tokens=1-2 delims==," %%I in (version.py) do ( if "%%I" == "VERSION " ( set version=%%J) )
+for /f "tokens=1-2 delims==, " %%I in (version.py) do ( if "%%I" == "VERSION" ( set version=%%J) )
 set version=%version:"=%
-set version=%version: =%
 cd ..\..\installer-source
 echo %version% > version.temp
 exit
