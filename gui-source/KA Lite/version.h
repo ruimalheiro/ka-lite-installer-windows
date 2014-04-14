522c211b623609773172e6d5816496ebfcb951bf
ruimalheiro <rmpmalheiro@gmail.com>
2014-04-14 22:16:35 +0100

    Fixed GUI version set.

--- gui-source/KA Lite/version.h
+++ gui-source/KA Lite/version.h
@@ -17,7 +17,6 @@ int getVersionFromFile(TCHAR * versionFinal)
 {
 	HANDLE hFile;
 	DWORD bytesRead = 0;
-	char versionBuffer[30];
 
 	hFile = CreateFile(L"ka-lite\\kalite\\version.py", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
@@ -28,41 +27,73 @@ int getVersionFromFile(TCHAR * versionFinal)
 		return 1;
 	}
 
-	if(ReadFile(hFile, versionBuffer, 29, &bytesRead, NULL) == FALSE)
+	LARGE_INTEGER fileSize = { 0 };
+	if(GetFileSizeEx(hFile, &fileSize) == 0)
+	{
+		MessageBox(NULL, L"Failed to get the size of the file.", L"Error", MB_OK | MB_ICONERROR);
+		CloseHandle(hFile);
+		return 1;
+	}
+
+	int SIZE = int(fileSize.LowPart);
+	char * versionBuffer = new char[SIZE];
+
+	if(ReadFile(hFile, versionBuffer, SIZE, &bytesRead, NULL) == FALSE)
 	{
 		MessageBox(NULL, L"Failed to read the config file", L"Error", MB_OK | MB_ICONERROR);
 		CloseHandle(hFile);
 		return 1;
 	}
 
-	if( bytesRead > 0 && bytesRead <= 29)
+	if(bytesRead == SIZE)
 	{
-		versionBuffer[29] = '\0';
-		int i = 0;
-		for(i=0; i<29; i++)
+		bool SEARCHING = TRUE;
+		bool READ_VERSION_FLAG = FALSE;
+		int INDEX = 0;
+		for (INDEX = 0; INDEX < SIZE; INDEX++)
 		{
-			if(versionBuffer[i] == '\"')
+			if(SEARCHING)
 			{
-				break;
-			}
-		}
-		i++;
-		int j = 0;
-		for(j=0;j<29; j++)
-		{
-			if(versionBuffer[i] == '\"')
-			{
-				break;
-			}
-			versionFinal[j] = versionBuffer[i];
-			i++;
-		}
-		versionFinal[j] = '\0';
+				if((INDEX+7) <= (SIZE-1))
+				{
+					if ((versionBuffer[INDEX] == 'V') &&
+						(versionBuffer[INDEX + 1] == 'E') &&
+						(versionBuffer[INDEX + 2] == 'R') &&
+						(versionBuffer[INDEX + 3] == 'S') &&
+						(versionBuffer[INDEX + 4] == 'I') &&
+						(versionBuffer[INDEX + 5] == 'O') &&
+						(versionBuffer[INDEX + 6] == 'N'))
+					{
+						READ_VERSION_FLAG = TRUE;
+					}
 
+					int CURRENT_INDEX = INDEX + 7;
+					int TEMP_INDEX = 0;
+					if (READ_VERSION_FLAG)
+					{
+						while(versionBuffer[CURRENT_INDEX] != '\n')
+						{
+							if((versionBuffer[CURRENT_INDEX] != ' ') &&
+							   (versionBuffer[CURRENT_INDEX] != '"') &&
+							   (versionBuffer[CURRENT_INDEX] != '='))
+							{
+								versionFinal[TEMP_INDEX] = versionBuffer[CURRENT_INDEX];
+								TEMP_INDEX++;
+							}
+							CURRENT_INDEX++;
+						}
+						READ_VERSION_FLAG = FALSE;
+						SEARCHING = FALSE;
+						versionFinal[TEMP_INDEX] = '\0';
+						break;
+					}
+				}
+			}
+		}
 		CloseHandle(hFile);
 		return 0;
 	}
-	else if( bytesRead == 0)
+	else if(bytesRead == 0)
 	{
 		MessageBox(NULL, L"No data read from file", L"Error", MB_OK | MB_ICONERROR);
 		CloseHandle(hFile);
